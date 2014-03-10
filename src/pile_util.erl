8eab12cb836a089f6fa25de272095b2aec72f6bb
Simon Unge <simonunge@gmail.com>
2014-03-10 20:50:44 +0100

    Refactored and test.

--- src/pile_util.erl
+++ src/pile_util.erl
@@ -12,12 +12,9 @@ move_from_pile_to_pile(Piles, From, To) ->
     Piles1 = lists:keyreplace(From, 1, Piles, {From, NewFromPile}),
     lists:keyreplace(To, 1, Piles1, {To, NewToPile}).
 
-add_one_card_to_each_pile([C1, C2, C3, C4], Piles) ->
-    Pile1 = proplists:get_value(pile1, Piles),
-    Pile2 = proplists:get_value(pile2, Piles),
-    Pile3 = proplists:get_value(pile3, Piles),
-    Pile4 = proplists:get_value(pile4, Piles),
-    Piles1 = lists:keyreplace(pile1, 1, Piles, {pile1,[C1 | Pile1]}),
-    Piles2 = lists:keyreplace(pile2, 1, Piles1, {pile2,[C2 | Pile2]}),
-    Piles3 = lists:keyreplace(pile3, 1, Piles2, {pile3,[C3 | Pile3]}),
-    lists:keyreplace(pile4, 1, Piles3, {pile4,[C4 | Pile4]}).
+add_one_card_to_each_pile(Cards, Piles) ->
+    lists:map(fun add_card_to_pile/1,
+              lists:zip(Cards,Piles)).
+
+add_card_to_pile({Card,{PileN,Pile}}) ->
+    {PileN, [Card | Pile]}.
--- test/pile_util_tests.erl
+++ test/pile_util_tests.erl
@@ -5,3 +5,9 @@
 move_from_pile_to_pile_test() ->
     L = [{pile1,[1,2,3]},{pile2,[4,5]}],
     ?assertEqual([{pile1,[2,3]},{pile2, [1,4,5]}],pile_util:move_from_pile_to_pile(L, pile1, pile2)).
+
+add_one_card_to_each_pile_test() ->
+    Piles = [{pile1,[]},{pile2,[]},{pile3,[]},{pile4,[]}],
+    FourCards = [{hearts,14},{diamonds,3},{diamonds,13},{clubs,5}],
+    ?assertEqual([{pile1,[{hearts,14}]},{pile2,[{diamonds,3}]},{pile3,[{diamonds,13}]},{pile4,[{clubs,5}]}],
+                pile_util:add_one_card_to_each_pile(FourCards, Piles)).

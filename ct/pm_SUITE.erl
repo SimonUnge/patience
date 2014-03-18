2f5c6703a7fe9fb4d4b5ca24d844b1992abbf64f
Simon Unge <simonunge@gmail.com>
2014-03-18 12:42:25 +0100

    Test that we do get the four top cards.

--- ct/pm_SUITE.erl
+++ ct/pm_SUITE.erl
@@ -4,7 +4,8 @@
 
 -export([all/0, groups/0, init_per_group/2, end_per_group/2]).
 -export([
-         get_four_empty_piles/1
+         get_four_empty_piles/1,
+         draw_first_turn_of_cards/1
         ]).
 
 -define(EMPTY_PILES,[{pile1, []}, {pile2, []}, {pile3, []}, {pile4, []}]).
@@ -19,13 +20,23 @@ groups() ->
      {pm_utils,
       [],
       [
-       get_four_empty_piles
+       get_four_empty_piles,
+       draw_first_turn_of_cards
       ]
      }
     ].
 
 get_four_empty_piles(_Config) ->
     ?EMPTY_PILES = patience_manager:show_piles().
+
+draw_first_turn_of_cards(_Config) ->
+    FullDeck = deck_manager:show_deck(),
+    [Card1,Card2,Card3,Card4] = lists:sublist(FullDeck,4),
+    FirstTurnPiles = [{pile1, [Card1]},
+                      {pile2, [Card2]},
+                      {pile3, [Card3]},
+                      {pile4, [Card4]}],
+    FirstTurnPiles = patience_manager:draw_cards().
 
 init_per_group(pm_utils, Config) ->
     start_app_return_config(Config);
